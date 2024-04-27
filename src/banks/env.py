28018b68cad21149281aa43b07ae1f4ae934ca85
Massimiliano Pippi <mpippi@gmail.com>
2024-04-27 16:33:52 +0200

    remove deprecate dependency

--- src/banks/env.py
+++ src/banks/env.py
@@ -2,13 +2,30 @@
 #
 # SPDX-License-Identifier: MIT
 import os
-from distutils.util import strtobool
 
 from jinja2 import Environment, select_autoescape
 
 from banks.extensions import GenerateExtension, HFInferenceEndpointsExtension
 from banks.filters import lemmatize
 from banks.loader import MultiLoader
+
+
+def strtobool(val: str) -> bool:
+    """Convert a string representation of truth to True or False.
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return True
+    elif val in ("n", "no", "f", "false", "off", "0"):
+        return False
+    else:
+        msg = f"invalid truth value {val}"
+        raise ValueError(msg)
+
 
 async_enabled = strtobool(os.environ.get("BANKS_ASYNC_ENABLED", "false"))
 

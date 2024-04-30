92df596beb291e852a38baf90c9e8382c5e83f52
Massimiliano Pippi <mpippi@gmail.com>
2024-04-30 08:43:22 +0200

    ignore deprecation warnings when running tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,8 @@
 # SPDX-FileCopyrightText: 2023-present Massimiliano Pippi <mpippi@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+import warnings
+
+# banks depends on modules producing loads of deprecation warnings, let's just ignore them,
+# nothing we can do anyways
+warnings.simplefilter("ignore", category=DeprecationWarning)

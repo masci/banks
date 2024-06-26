96b3b218ef014658f6e94b69fa4b6e20e3717a55
Massimiliano Pippi <mpippi@gmail.com>
2024-06-26 07:17:57 -0700

    reformat

--- src/banks/extensions/generate.py
+++ src/banks/extensions/generate.py
@@ -49,9 +49,7 @@ class GenerateExtension(Extension):
             args.append(nodes.Const(None))
 
         if parser.environment.is_async:
-            return nodes.Output([self.call_method("_agenerate", args)]).set_lineno(
-                lineno
-            )
+            return nodes.Output([self.call_method("_agenerate", args)]).set_lineno(lineno)
         return nodes.Output([self.call_method("_generate", args)]).set_lineno(lineno)
 
     def _generate(self, text, model_name=DEFAULT_MODEL):
@@ -64,9 +62,7 @@ class GenerateExtension(Extension):
             {"role": "system", "content": SYSTEM_PROMPT.text()},
             {"role": "user", "content": text},
         ]
-        response: ModelResponse = cast(
-            ModelResponse, completion(model=model_name, messages=messages)
-        )
+        response: ModelResponse = cast(ModelResponse, completion(model=model_name, messages=messages))
         return self._get_content(response)
 
     async def _agenerate(self, text, model_name=DEFAULT_MODEL):
@@ -79,9 +75,7 @@ class GenerateExtension(Extension):
             {"role": "system", "content": SYSTEM_PROMPT.text()},
             {"role": "user", "content": text},
         ]
-        response: ModelResponse = cast(
-            ModelResponse, await acompletion(model=model_name, messages=messages)
-        )
+        response: ModelResponse = cast(ModelResponse, await acompletion(model=model_name, messages=messages))
         return self._get_content(response)
 
     def _get_content(self, response: ModelResponse) -> str:

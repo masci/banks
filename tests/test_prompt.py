2c8a7039847e62ad159871d0e75ac6bcb7800018
Massimiliano Pippi <mpippi@gmail.com>
2024-09-28 14:25:35 +0200

    feat: add meta and version to the Prompt class

--- tests/test_prompt.py
+++ tests/test_prompt.py
@@ -1,9 +1,12 @@
 from unittest import mock
 
+import pytest
 import regex as re
+from jinja2 import Environment
 
-from banks import Prompt
+from banks import AsyncPrompt, Prompt
 from banks.cache import DefaultCache
+from banks.errors import AsyncError
 
 
 def test_canary_word_generation():
@@ -25,3 +28,44 @@ def test_prompt_cache():
     p = Prompt("This is my prompt", render_cache=mock_cache)
     p.text()
     mock_cache.set.assert_called_once()
+
+
+def test_ctor():
+    p = Prompt(
+        text="This is raw text",
+        version="1.0",
+        metadata={"LLM": "GPT-3.5"},
+        canary_word="FOO",
+        render_cache=DefaultCache(),
+    )
+    assert p.raw == "This is raw text"
+    assert p.version == "1.0"
+    assert p.metadata == {"LLM": "GPT-3.5"}
+    assert p.canary_leaked("The message is FOO")
+    assert p.text() == "This is raw text"
+    assert p.text() == "This is raw text"
+
+
+def test_ctor_async_disabled():
+    with mock.patch("banks.prompt.config", ASYNC_ENABLED=False):
+        with pytest.raises(AsyncError):
+            AsyncPrompt(text="This is raw text")
+
+
+@pytest.mark.asyncio
+async def test_ctor_async():
+    with mock.patch("banks.prompt.config", ASYNC_ENABLED=True):
+        p = AsyncPrompt(
+            text="This is raw text",
+            version="1.0",
+            metadata={"LLM": "GPT-3.5"},
+            canary_word="FOO",
+            render_cache=DefaultCache(),
+        )
+        p._template = Environment(autoescape=True, enable_async=True).from_string(p.raw)
+        assert p.raw == "This is raw text"
+        assert p.version == "1.0"
+        assert p.metadata == {"LLM": "GPT-3.5"}
+        assert p.canary_leaked("The message is FOO")
+        assert await p.text() == "This is raw text"
+        assert await p.text() == "This is raw text"

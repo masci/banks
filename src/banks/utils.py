b715793d09b9d41f013babf496f728e468e44531
Massimiliano Pippi <mpippi@gmail.com>
2024-10-14 09:09:49 +0200

    add introspection utils

--- src/banks/utils.py
+++ src/banks/utils.py
@@ -1,5 +1,7 @@
 import secrets
 
+from griffe import Docstring, parse_google, parse_numpy, parse_sphinx
+
 
 def strtobool(val: str) -> bool:
     """
@@ -22,3 +24,42 @@ def strtobool(val: str) -> bool:
 
 def generate_canary_word(prefix: str = "BANKS[", suffix: str = "]", token_length: int = 8) -> str:
     return f"{prefix}{secrets.token_hex(token_length // 2)}{suffix}"
+
+
+def python_type_to_jsonschema(python_type: type) -> str:
+    """Given a Python type, returns the jsonschema string describing it."""
+    if python_type is str:
+        return "string"
+    elif python_type is int:
+        return "integer"
+    elif python_type is float:
+        return "number"
+    elif python_type is bool:
+        return "boolean"
+    elif python_type is list:
+        return "array"
+    elif python_type is dict:
+        return "object"
+    else:
+        msg = f"Unsupported type: {python_type}"
+        raise ValueError(msg)
+
+
+def parse_params_from_docstring(docstring: str) -> dict[str, dict[str, str]]:
+    param_docs = []
+    ds = Docstring(docstring)
+    for parser in (parse_google, parse_numpy, parse_sphinx):
+        sections = parser(ds)
+        for section in sections:
+            if section.kind == "parameters":
+                param_docs = section.value
+                break
+        if param_docs:
+            break
+
+    ret = {}
+    for d in param_docs:
+        d_dict = d.as_dict()
+        ret[d_dict.pop("name")] = d_dict
+
+    return ret

98248a405fd4f874c991deb43605a6d115bfde7f
Massimiliano Pippi <mpippi@gmail.com>
2024-11-10 18:50:49 +0100

    feat: remove deprecated extension `HFInferenceEndpointsExtension` (#23)
    * remove deprecated extension
    
    * remove exception

--- src/banks/extensions/__init__.py
+++ src/banks/extensions/__init__.py
@@ -2,6 +2,5 @@
 #
 # SPDX-License-Identifier: MIT
 from banks.extensions.generate import GenerateExtension
-from banks.extensions.inference_endpoint import HFInferenceEndpointsExtension
 
-__all__ = ("GenerateExtension", "HFInferenceEndpointsExtension")
+__all__ = ("GenerateExtension",)

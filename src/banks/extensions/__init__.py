b84f584947e329f5b11833b371e648198242bda5
Massimiliano Pippi <mpippi@gmail.com>
2024-11-30 17:47:37 +0100

    remove generate module

--- src/banks/extensions/__init__.py
+++ src/banks/extensions/__init__.py
@@ -1,6 +1,3 @@
 # SPDX-FileCopyrightText: 2023-present Massimiliano Pippi <mpippi@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-from banks.extensions.generate import GenerateExtension
-
-__all__ = ("GenerateExtension",)

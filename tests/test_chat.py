57e8f3bb4e7a93ae7a4580ed639dfe03d4529213
Massimiliano Pippi <mpippi@gmail.com>
2025-03-01 15:25:41 +0100

    fix: fix interleaved blocks in chat messages (#32)
    * fix: fix interleaved blocks in chat messages
    
    * fix unit tests
    
    * linting

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -17,3 +17,13 @@ def test_wrong_tag_params():
 def test_wrong_role_type():
     with pytest.raises(TemplateSyntaxError):
         Prompt('{% chat role="does not exist" %}{% endchat %}')
+
+
+def test_blocks():
+    p = Prompt('{% chat role="user" %}Tell me what you see: {{ picture | image }}{% endchat %}')
+    messages = p.chat_messages({"picture": "http://foo.bar"})
+    assert len(messages) == 1
+    content = messages[0].content
+    assert len(content) == 2
+    assert content[0].type == "text"  # type: ignore
+    assert content[1].type == "image_url"  # type: ignore

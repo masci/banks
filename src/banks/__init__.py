d40f3cf8da4db5a292663a4e501a95fd170b2045
Logan Markewich <logan.markewich@live.com>
2025-03-11 14:10:20 +0000

    future annotations import

--- src/banks/__init__.py
+++ src/banks/__init__.py
@@ -1,10 +1,12 @@
 # SPDX-FileCopyrightText: 2023-present Massimiliano Pippi <mpippi@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+from __future__ import annotations
+
 from .config import config
 from .env import env
 from .prompt import AsyncPrompt, Prompt
 from .types import ChatMessage
-from __future__ import annotations
+
 
 __all__ = ("env", "Prompt", "AsyncPrompt", "config", "ChatMessage")
